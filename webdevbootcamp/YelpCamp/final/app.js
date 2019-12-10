bd56c138ee14abf486b942777164b5f009b8bcec
Ouriel91 <orieloh91@gmail.com>
2019-12-10 14:34:08 +0000

    Fix bug in admin feature

--- webdevbootcamp/YelpCamp/final/app.js
+++ webdevbootcamp/YelpCamp/final/app.js
@@ -20,7 +20,7 @@ var commentsRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes			= require("./routes/index");
 
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v10_1";
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_final";
 
 mongoose.connect(url, 
 				 {useUnifiedTopology: true, 
